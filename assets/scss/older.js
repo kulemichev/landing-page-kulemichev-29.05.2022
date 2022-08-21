d342a3f38f1a8adf4e4f7a34f690d670f7b327b3
Alex Kulemichev <alexey.kulemichev@gmail.com>
2022-08-21 09:10:47 +0300

    Added main.js to the project

---
+++ assets/scss/older.js
@@ -0,0 +1,14 @@
+// // const print = function () {
+// //   console.log('ALEX');
+// //   setTimeout(function () {
+// //     console.log('PRIVET');
+// //   }, 2000);
+// // }
+// // print();
+
+// function multArg(val, mult = 1) {
+//   let res = val * mult;
+//   console.log(res);
+// }
+// multArg(4, 4);
+// multArg(4);
